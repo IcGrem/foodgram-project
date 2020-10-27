53b1b436d4b654cb63deb28b9fd3fb1e0c3a3f4e
Sergei Petukhov <Sergei.Petukhov@gmail.com>
2020-10-27 19:12:38 +0300

    cart_to_pdf repair

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# FONTS = '/static/fonts/'
+FONTS = '/static/fonts/'
 
 STATIC_URL = '/static/'
 if DEBUG:
